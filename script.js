6b4e0e320b8e9b0f523dd6f9d684685c17ded784
Guilherme dos Santos Pineda <101909863+Guilherme-dos-Santos-Pineda@users.noreply.github.com>
2022-12-28 12:40:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const lista = document.querySelector("#lista");
+const nav = document.querySelector(".navigation");
+
+// eventos
+
+lista.addEventListener("click", ()=>{
+    nav.classList.toggle("aparecer")
+
+})
